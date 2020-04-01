37a5527a1d4d72c8a4578c679f5db597a766a3b7
Jan-E <github@ehrhardt.nl>
2020-04-01 13:31:14 +0200

    Build.cmd: Add python gen-make

---
+++ Build.cmd
@@ -0,0 +1,2 @@
+cd \svn\subversion-%svn_version%
+python gen-make.py --release -t vcproj --vsnet-version=2017 --with-apr-util=C:\Apache24 --with-apr=C:\Apache24 --with-apr-iconv=C:\Apache24 --with-apr_memcache=C:\Apache24 --with-httpd=C:\Apache24 --with-openssl=C:\Apache24 --with-zlib=C:\Apache24 --with-sqlite=C:\Apache24
