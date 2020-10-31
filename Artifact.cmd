f9bbdba95a021a40583066957434ea696c4f96f5
Jan-E <github@ehrhardt.nl>
2020-10-31 19:09:08 +0100

    Add libapriconv-1.dll to Artifact

--- Artifact.cmd
+++ Artifact.cmd
@@ -6,4 +6,6 @@ for /r %%f in (*.dll, *.exe) do @copy "%%f" \svn\bin /y
 for /r %%f in (*.lib) do @copy "%%f" \svn\lib /y
 for /r %%f in (*.so) do @copy "%%f" \svn\modules /y
 cd \svn
+copy \Apache24\bin\libapr*.dll \svn\bin\ /y
+copy \Apache24\lib\libapr*.lib \svn\lib\ /y
 7z a svn-%svn_version%-%vcversion%-%platform%.zip bin/* lib/* modules/*
