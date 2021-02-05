31dee1d0f63c514688e83526c633af00397ed70a
Jan-E <github@ehrhardt.nl>
2021-02-05 08:56:41 +0100

    Subversion 1.14.1 up for testing/signing - no clean on 2nd

--- Build.cmd
+++ Build.cmd
@@ -18,9 +18,9 @@ if "%platform%"=="x86" set lib=C:\OpenSSL-v111-Win32\lib;\downloads\zlib-%zlib_v
 if "%platform%"=="x86" set include=C:\OpenSSL-v111-Win32\include;\downloads\zlib-%zlib_version%;\Apache24\include;%include%
 
 cd \downloads\serf-1.3.9
-                                              nmake /s /nologo /f \svn\serf.mak CLEAN APR_SRC=C:\Apache24 APRUTIL_SRC=C:\Apache24 ZLIB_SRC=C:\Apache24 OPENSSL_SRC=C:\Apache24 || exit 0
-                       if "%platform%"=="x64" nmake /s /nologo /f \svn\serf.mak ALL APR_SRC=C:\Apache24 APRUTIL_SRC=C:\Apache24 ZLIB_SRC=C:\Apache24 OPENSSL_SRC=C:\OpenSSL-v111-Win64
-                       if "%platform%"=="x86" nmake /s /nologo /f \svn\serf.mak ALL APR_SRC=C:\Apache24 APRUTIL_SRC=C:\Apache24 ZLIB_SRC=C:\Apache24 OPENSSL_SRC=C:\OpenSSL-v111-Win32
+if "%run_clean%"=="yes" nmake /s /nologo /f \svn\serf.mak CLEAN APR_SRC=C:\Apache24 APRUTIL_SRC=C:\Apache24 ZLIB_SRC=C:\Apache24 OPENSSL_SRC=C:\Apache24 || exit 0
+if "%platform%"=="x64"  nmake /s /nologo /f \svn\serf.mak ALL APR_SRC=C:\Apache24 APRUTIL_SRC=C:\Apache24 ZLIB_SRC=C:\Apache24 OPENSSL_SRC=C:\OpenSSL-v111-Win64
+if "%platform%"=="x86"  nmake /s /nologo /f \svn\serf.mak ALL APR_SRC=C:\Apache24 APRUTIL_SRC=C:\Apache24 ZLIB_SRC=C:\Apache24 OPENSSL_SRC=C:\OpenSSL-v111-Win32
 if exist Release\serf-1.lib copy Release\serf-1.lib . /y
 
 cd \downloads\subversion-%svn_version%
@@ -29,12 +29,12 @@ if "%run_build%"=="no" exit 0
 
 if "%platform%"=="x64" echo C:\Python27-x64\python gen-make.py --release -t vcproj --vsnet-version=2017 --with-apr-util=C:\Apache24 --with-apr=C:\Apache24 --with-apr-iconv=C:\Apache24 --with-apr_memcache=C:\Apache24 --with-httpd=C:\Apache24 --with-openssl=C:\OpenSSL-v111-Win64 --with-zlib=C:\Apache24 --with-sqlite=C:\downloads\sqlite-amalgamation-%sqlite_version% --with-jdk="C:\Program Files\Java\jdk9" --with-serf=\downloads\serf-1.3.9
 if "%platform%"=="x64"      C:\Python27-x64\python gen-make.py --release -t vcproj --vsnet-version=2017 --with-apr-util=C:\Apache24 --with-apr=C:\Apache24 --with-apr-iconv=C:\Apache24 --with-apr_memcache=C:\Apache24 --with-httpd=C:\Apache24 --with-openssl=C:\OpenSSL-v111-Win64 --with-zlib=C:\Apache24 --with-sqlite=C:\downloads\sqlite-amalgamation-%sqlite_version% --with-jdk="C:\Program Files\Java\jdk9" --with-serf=\downloads\serf-1.3.9
-if "%platform%"=="x64" devenv subversion_vcnet.sln /Clean "Release|x64" || exit 0
-if "%platform%"=="x64" devenv subversion_vcnet.sln /Clean "Release|Win32" || exit 0
-if "%platform%"=="x64" devenv subversion_vcnet.sln /Build "Release|x64" || exit 0
+if "%platform%"=="x64" if "%run_clean%"=="yes" devenv subversion_vcnet.sln /Clean "Release|x64" || exit 0
+if "%platform%"=="x64" if "%run_clean%"=="yes" devenv subversion_vcnet.sln /Clean "Release|Win32" || exit 0
+if "%platform%"=="x64"                         devenv subversion_vcnet.sln /Build "Release|x64" || exit 0
 
 if "%platform%"=="x86" echo C:\Python27\python gen-make.py --release -t vcproj --vsnet-version=2017 --with-apr-util=C:\Apache24 --with-apr=C:\Apache24 --with-apr-iconv=C:\Apache24 --with-apr_memcache=C:\Apache24 --with-httpd=C:\Apache24 --with-openssl=C:\OpenSSL-v111-Win32 --with-zlib=C:\Apache24 --with-sqlite=C:\downloads\sqlite-amalgamation-%sqlite_version% --with-jdk="C:\Program Files\Java\jdk9" --with-serf=\downloads\serf-1.3.9
 if "%platform%"=="x86"      C:\Python27\python gen-make.py --release -t vcproj --vsnet-version=2017 --with-apr-util=C:\Apache24 --with-apr=C:\Apache24 --with-apr-iconv=C:\Apache24 --with-apr_memcache=C:\Apache24 --with-httpd=C:\Apache24 --with-openssl=C:\OpenSSL-v111-Win32 --with-zlib=C:\Apache24 --with-sqlite=C:\downloads\sqlite-amalgamation-%sqlite_version% --with-jdk="C:\Program Files\Java\jdk9" --with-serf=\downloads\serf-1.3.9
-if "%platform%"=="x86" devenv subversion_vcnet.sln /Clean "Release|x64" || exit 0
-if "%platform%"=="x86" devenv subversion_vcnet.sln /Clean "Release|Win32" || exit 0
-if "%platform%"=="x86" devenv subversion_vcnet.sln /Build "Release|Win32" || exit 0
+if "%platform%"=="x86" if "%run_clean%"=="yes" devenv subversion_vcnet.sln /Clean "Release|x64" || exit 0
+if "%platform%"=="x86" if "%run_clean%"=="yes" devenv subversion_vcnet.sln /Clean "Release|Win32" || exit 0
+if "%platform%"=="x86"                         devenv subversion_vcnet.sln /Build "Release|Win32" || exit 0
