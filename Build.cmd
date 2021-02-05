ca17d258e7c82cf1d2bd01077a8f6ab09f36b388
Jan-E <github@ehrhardt.nl>
2021-02-05 06:54:19 +0100

    Subversion 1.14.1 up for testing/signing

--- Build.cmd
+++ Build.cmd
@@ -19,7 +19,7 @@ if "%platform%"=="x86" set include=C:\OpenSSL-v111-Win32\include;\downloads\zlib
 
 cd \downloads\serf-1.3.9
                                               nmake /s /nologo /f \svn\serf.mak CLEAN APR_SRC=C:\Apache24 APRUTIL_SRC=C:\Apache24 ZLIB_SRC=C:\Apache24 OPENSSL_SRC=C:\Apache24 || exit 0
-if "%run_tests%"=="no" if "%platform%"=="x64" nmake /s /nologo /f \svn\serf.mak ALL APR_SRC=C:\Apache24 APRUTIL_SRC=C:\Apache24 ZLIB_SRC=C:\Apache24 OPENSSL_SRC=C:\OpenSSL-v111-Win64
+                       if "%platform%"=="x64" nmake /s /nologo /f \svn\serf.mak ALL APR_SRC=C:\Apache24 APRUTIL_SRC=C:\Apache24 ZLIB_SRC=C:\Apache24 OPENSSL_SRC=C:\OpenSSL-v111-Win64
                        if "%platform%"=="x86" nmake /s /nologo /f \svn\serf.mak ALL APR_SRC=C:\Apache24 APRUTIL_SRC=C:\Apache24 ZLIB_SRC=C:\Apache24 OPENSSL_SRC=C:\OpenSSL-v111-Win32
 if exist Release\serf-1.lib copy Release\serf-1.lib . /y
 
@@ -29,8 +29,8 @@ if "%run_build%"=="no" exit 0
 
 if "%platform%"=="x64" echo C:\Python27-x64\python gen-make.py --release -t vcproj --vsnet-version=2017 --with-apr-util=C:\Apache24 --with-apr=C:\Apache24 --with-apr-iconv=C:\Apache24 --with-apr_memcache=C:\Apache24 --with-httpd=C:\Apache24 --with-openssl=C:\OpenSSL-v111-Win64 --with-zlib=C:\Apache24 --with-sqlite=C:\downloads\sqlite-amalgamation-%sqlite_version% --with-jdk="C:\Program Files\Java\jdk9" --with-serf=\downloads\serf-1.3.9
 if "%platform%"=="x64"      C:\Python27-x64\python gen-make.py --release -t vcproj --vsnet-version=2017 --with-apr-util=C:\Apache24 --with-apr=C:\Apache24 --with-apr-iconv=C:\Apache24 --with-apr_memcache=C:\Apache24 --with-httpd=C:\Apache24 --with-openssl=C:\OpenSSL-v111-Win64 --with-zlib=C:\Apache24 --with-sqlite=C:\downloads\sqlite-amalgamation-%sqlite_version% --with-jdk="C:\Program Files\Java\jdk9" --with-serf=\downloads\serf-1.3.9
-if "%run_tests%"=="no" if "%platform%"=="x64" devenv subversion_vcnet.sln /Clean "Release|x64" || exit 0
-if "%run_tests%"=="no" if "%platform%"=="x64" devenv subversion_vcnet.sln /Clean "Release|Win32" || exit 0
+if "%platform%"=="x64" devenv subversion_vcnet.sln /Clean "Release|x64" || exit 0
+if "%platform%"=="x64" devenv subversion_vcnet.sln /Clean "Release|Win32" || exit 0
 if "%platform%"=="x64" devenv subversion_vcnet.sln /Build "Release|x64" || exit 0
 
 if "%platform%"=="x86" echo C:\Python27\python gen-make.py --release -t vcproj --vsnet-version=2017 --with-apr-util=C:\Apache24 --with-apr=C:\Apache24 --with-apr-iconv=C:\Apache24 --with-apr_memcache=C:\Apache24 --with-httpd=C:\Apache24 --with-openssl=C:\OpenSSL-v111-Win32 --with-zlib=C:\Apache24 --with-sqlite=C:\downloads\sqlite-amalgamation-%sqlite_version% --with-jdk="C:\Program Files\Java\jdk9" --with-serf=\downloads\serf-1.3.9
